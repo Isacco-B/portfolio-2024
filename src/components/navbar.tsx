ec963ead3e1cbe73fc904972d961388a427afbd3
Isacco-B <git@isaccobertoli.com>
2024-09-30 16:37:44 +0200

    link target

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -45,6 +45,7 @@ export default function Navbar() {
                 <TooltipTrigger asChild>
                   <Link
                     href={social.url}
+                    target="_blank"
                     className={cn(
                       buttonVariants({ variant: "ghost", size: "icon" }),
                       "size-12"
