1a1d76bd2d5ed14cf996d1c6b68ff1b73bfdd554
Isacco-B <git@isaccobertoli.com>
2024-12-15 00:17:30 +0100

    remove expanded option

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -3,10 +3,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardHeader } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
-import { ChevronRightIcon } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 
 interface ResumeCardProps {
@@ -14,103 +10,58 @@ interface ResumeCardProps {
   altText: string;
   title: string;
   subtitle?: string;
-  href?: string;
   badges?: readonly string[];
   period?: string;
   description?: string;
-  showDescription?: boolean;
 }
 export const ResumeCard = ({
   logoUrl,
   altText,
   title,
   subtitle,
-  href,
   badges,
   period,
   description,
-  showDescription = false,
 }: ResumeCardProps) => {
-  const [isExpanded, setIsExpanded] = React.useState(showDescription);
-
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault();
-    if (description && !showDescription) {
-      setIsExpanded(!isExpanded);
-    }
-  };
-
   return (
-    <Link
-      href={href || "#"}
-      className="block cursor-pointer"
-      onClick={handleClick}
-    >
-      <Card className="flex p-4">
-        <div className="flex-none">
-          <Avatar className="size-12 m-auto bg-muted-background dark:bg-foreground">
-            <AvatarImage
-              src={logoUrl}
-              alt={altText}
-              className="object-contain"
-            />
-            <AvatarFallback>{altText[0]}</AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex-grow ml-4 items-center flex-col group">
-          <CardHeader>
-            <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
-                {title}
-                {badges && (
-                  <span className="inline-flex gap-x-1">
-                    {badges.map((badge, index) => (
-                      <Badge
-                        variant="secondary"
-                        className="align-middle text-xs"
-                        key={index}
-                      >
-                        {badge}
-                      </Badge>
-                    ))}
-                  </span>
-                )}
-                {!showDescription && (
-                  <ChevronRightIcon
-                    className={cn(
-                      "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
-                      isExpanded ? "rotate-90" : "rotate-0"
-                    )}
-                  />
-                )}
-              </h3>
-              {period && (
-                <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
-                  {period}
-                </div>
+    <Card className="flex p-4">
+      <div className="flex-none">
+        <Avatar className="size-12 m-auto bg-muted-background dark:bg-foreground">
+          <AvatarImage src={logoUrl} alt={altText} className="object-contain" />
+          <AvatarFallback>{altText[0]}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex-grow ml-4 items-center flex-col group">
+        <CardHeader>
+          <div className="flex items-center justify-between gap-x-2 text-base">
+            <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+              {title}
+              {badges && (
+                <span className="inline-flex gap-x-1">
+                  {badges.map((badge, index) => (
+                    <Badge
+                      variant="secondary"
+                      className="align-middle text-xs"
+                      key={index}
+                    >
+                      {badge}
+                    </Badge>
+                  ))}
+                </span>
               )}
-            </div>
-            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
-          </CardHeader>
-          {description && (
-            <motion.div
-              initial={{ opacity: 0, height: 0 }}
-              animate={{
-                opacity: isExpanded ? 1 : 0,
-
-                height: isExpanded ? "auto" : 0,
-              }}
-              transition={{
-                duration: 0.7,
-                ease: [0.16, 1, 0.3, 1],
-              }}
-              className="mt-2 text-xs sm:text-sm"
-            >
-              {description}
-            </motion.div>
-          )}
-        </div>
-      </Card>
-    </Link>
+            </h3>
+            {period && (
+              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+                {period}
+              </div>
+            )}
+          </div>
+          {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
+        </CardHeader>
+        {description && (
+          <div className="mt-2 text-xs sm:text-sm">{description}</div>
+        )}
+      </div>
+    </Card>
   );
 };
